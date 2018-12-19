1686ba3f555cc22792184fca967b84e8d685f2a5
Martin Wawro <martin.wawro@gmail.com>
2018-12-19 10:56:25 +0100

    Path correction

--- gather_sdk.sh
+++ gather_sdk.sh
@@ -20,12 +20,12 @@ install -d -m 755 sdk/native/3rdparty/libs/armeabi-v7a
 
 cp -r $SRC64/sdk/etc/* sdk/etc/
 cp -r $SRC64/sdk/native/jni/include sdk/native/jni/
-cp -r $SRC64/sdk/native/staticlibs/arm64-v8a/* sdk/native/libs/arm64-v8a/
+cp -r $SRC64/sdk/native/libs/arm64-v8a/* sdk/native/libs/arm64-v8a/
 cp -r $SRC64/sdk/native/3rdparty/libs/arm64-v8a/* sdk/native/3rdparty/libs/arm64-v8a
 cp $SRC64/sdk/native/jni/OpenCV.mk sdk/native/jni/
 cp $SRC64/sdk/native/jni/OpenCV-arm64-v8a.mk sdk/native/jni/
 
-cp -r $SRC32/sdk/native/staticlibs/armeabi-v7a/* sdk/native/libs/armeabi-v7a/
+cp -r $SRC32/sdk/native/libs/armeabi-v7a/* sdk/native/libs/armeabi-v7a/
 cp -r $SRC32/sdk/native/3rdparty/libs/armeabi-v7a/* sdk/native/3rdparty/libs/armeabi-v7a
 cp $SRC32/sdk/native/jni/OpenCV-armeabi-v7a.mk sdk/native/jni/
 
